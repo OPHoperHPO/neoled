5454ba0b62f52b3c22401bb9a5e8a6460da58503
Anodev <farvard34@gmail.com>
2020-01-02 15:20:38 +1000

    Update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# neoled
+# ESP32 Smart Neopixel Light
 ESP32 Micropython smart led with OTA and other stuff
+# Don't clone this repo, because firmware is not done and don't work AT THIS MOMENT!
--- server/ota/dev/0-10-dev/firmware/lib/ota.py
+++ server/ota/dev/0-10-dev/firmware/lib/ota.py
@@ -3,6 +3,7 @@ import os
 import json
 import requests
 
+
 class OTA:
     def __init__(self, config):
         """OTA object"""
@@ -11,7 +12,7 @@ class OTA:
 
     def check(self):
         """Checks for update"""
-        versions_path = self.server_url+'/server/ota/versions.json'
+        versions_path = self.server_url + '/server/ota/versions.json'
         if self.__ping__(self.server_url):
             content = self.__download_content__(versions_path)
             if content:
@@ -30,32 +31,43 @@ class OTA:
             return False
 
     def __backup__(self):
+        """Backup firmware"""
         try:
+            # Define paths
             path = '/flash/'
-            backup_folder = path+"_backup_"
+            backup_folder = path + "_backup_"
             try:
-                os.mkdir(backup_folder)
+                os.mkdir(backup_folder)  # Try to create folder
             except BaseException as e:
                 pass
-            all_files = os.listdir(path)
+            all_files = os.listdir('/flash')  # Get list of all files and folders
             try:
-                all_files.remove("_backup_")
-                all_files.remove("config.py")
+                all_files.remove("_backup_")  # Remove backup folder from filelist
+                all_files.remove("config.py")  # Remove config file from filelist
             except BaseException as e:
-                pass
+                return False
             for file in all_files:
-                try:
-                    new = path+file+'/'
-                    backup_path = backup_folder+'/'+file
-                    alf = os.listdir(path+file)
+                try:  # Check for folder
+                    new = path + file + '/'  # Define path to folder
+                    backup_path = backup_folder + '/' + file  # Define path to folder in backup_folder
+                    alf = os.listdir(path + file)  # Get list of all files in folder
+                    # or Handle Exception if it is path to file
                     try:
-                        os.mkdir(backup_path)
+                        os.mkdir(backup_path)  # Try to create dir in backup folder
                     except BaseException as e:
                         pass
-                    for f in alf:
-                        os.rename(new+f, backup_path+'/'+f)
-                except BaseException as e:
-                    os.rename(path+file, backup_folder+'/'+file)
+                    for f in alf:  # For every file in this dir, we move this file
+                        try:
+                            os.rename(new + f, backup_path + '/' + f)
+                        except BaseException:
+                            os.remove(backup_path + '/' + f)  # Remove old backup
+                            os.rename(new + f, backup_path + '/' + f)  # Move in backup
+                except BaseException as e:  # If it is file
+                    try:
+                        os.rename(path + file, backup_folder + '/' + file)  # Move this file
+                    except BaseException as e:
+                        os.remove(backup_folder + '/' + file)  # Remove this file in backup
+                        os.rename(path + file, backup_folder + '/' + file)  # Again try to move
             return True
         except BaseException as E:
             return False
@@ -63,9 +75,9 @@ class OTA:
     def update(self, new_version):
         """Update"""
         print("update", new_version)
-        version_url = self.server_url+'/server/ota/{}/{}'.format(self.branch, new_version)
-        firmware_url = version_url+'/firmware/'
-        files_url = version_url+"/files.json"
+        version_url = self.server_url + '/server/ota/{}/{}'.format(self.branch, new_version)
+        firmware_url = version_url + '/firmware/'
+        files_url = version_url + "/files.json"
         if self.__ping__(self.server_url):
             stat = self.__backup__()
             if stat:
@@ -100,28 +112,37 @@ class OTA:
             return False
 
     def __restore__(self):
+        """Restore firmware"""
         try:
-            path = '/flash/'
-            backup_folder = path + "_backup_"
-            all_files = os.listdir(backup_folder)
-            try:
-                all_files.remove("_backup_")
-                all_files.remove("config.py")
-            except BaseException as e:
-                pass
-            for file in all_files:
-                try:
-                    new = backup_folder+'/'+ file + '/'
-                    backup_path = path + file
-                    alf = os.listdir(backup_folder+'/'+ file)
+            # Define paths
+            path = '/flash'
+            backup_folder = path + '/' + "_backup_"
+            backup_filelist = os.listdir(backup_folder)  # Get list of all files and folders
+            for file in backup_filelist:
+                # Define paths
+                backup_filepath = backup_folder + '/' + file
+                original_filepath = path + '/' + file
+                try:  # Check for folder
+                    bkp_filelist_f2 = os.listdir(backup_filepath)  # Get list of all files in folder
+                    # or Handle Exception if it is path to file
                     try:
-                        os.mkdir(backup_path)
+                        os.mkdir(original_filepath)  # Try to create dir in path
                     except BaseException as e:
                         pass
-                    for f in alf:
-                        os.rename(new + f, backup_path + '/' + f)
-                except BaseException as e:
-                    os.rename(backup_folder+'/' + file, path + file)
+                    for file2 in bkp_filelist_f2:  # For every file in this dir, we move this file
+                        path_2 = backup_filepath+'/'+file2
+                        org_path_2 = original_filepath+'/'+file2
+                        try:
+                            os.rename(path_2, org_path_2)
+                        except BaseException as g:
+                            os.remove(org_path_2)  # Remove new corrupted file
+                            os.rename(path_2, org_path_2)  # Move from backup
+                except BaseException as e:  # If it is file
+                    try:
+                        os.rename(backup_filepath, original_filepath)  # Move this file
+                    except BaseException as e:
+                        os.remove(original_filepath)  # Remove new corrupted file
+                        os.rename(backup_filepath, original_filepath)   # Again try to move
             return True
         except BaseException as E:
             return False
